a731768b7cb35da00d32a9b8d1a971dda086e0e5
Alexander Grothe <seahawk1986@gmx.de>
2017-02-28 16:07:10 +0100

    remove lirc_pci_id.sh

--- roles/yavdr-common/files/list_pci_ids.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# return all vendor:device ids
-# typical device path:
-# /sys/devices/pci0000\:00/0000\:00\:1f.2/
-
-for device in /sys/devices/pci*/*:*:*/; do
-  echo "$(cat "$device"/vendor):$(cat "$device"/device)" | sed 's/0x//g'
-done
-
