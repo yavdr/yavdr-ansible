bae1028bc79ed55a49ce2d44eb00cd6e6f5c71ba
Alexander Grothe <seahawk1986@gmx.de>
2017-02-28 16:31:43 +0100

    relocate install-yavdr.sh

--- install-yavdr.sh
+++ install-yavdr.sh
@@ -4,9 +4,10 @@ if (( $EUID != 0 )); then
     exit
 fi
 
+apt-get -y install software-properties-common
 # Add repository for ansible
 add-apt-repository -y ppa:ansible/ansible
 # update packages
 apt-get update
 # install required packages
-apt-get -y install ansible libyaml-0-2 python-crypto python-ecdsa python-httplib2 python-jinja2 python-markupsafe python-paramiko python-pkg-resources python-setuptools python-six python-yaml sshpass
+apt-get -y install ansible python
