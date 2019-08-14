aea0963c67111aaf974dd40b3bf31c2747b6da53
Alexander Grothe <seahawk1986@gmx.de>
2019-08-14 08:32:25 +0200

    Add .dir-locals.el and .gitignore

---
+++ .dir-locals.el
@@ -0,0 +1,10 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+;((magit-stash-mode
+;  (magit-todos-exclude-globs ("*.html" "*.tex" "*.pygtex" "*~"))))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((magit-status-mode
+  (magit-todos-exclude-globs "*.html" "*~" "*.tex")))
