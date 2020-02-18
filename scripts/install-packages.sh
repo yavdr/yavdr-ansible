6b33db03cc9789d2f15978955b2450680d137d7c
Alexander Grothe <seahawk1986@gmx.de>
2020-02-18 18:25:51 +0100

    remove client-side packages which are installed by the playbook

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -2,9 +2,7 @@
 required_packages=( 
     ansible
     build-essential
-    python3-kmodpy
     python3-argcomplete
-    python3-mysqldb
     python3-venv
     python3-wheel
     software-properties-common
