eae28c03e12fce29252371fd474fe9616cea20b0
Alexander Grothe <seahawk1986@gmx.de>
2020-07-10 14:20:22 +0200

    Use proper shebang and update Manual.org

--- roles/vdr/files/reccmds/removeindex.sh
+++ roles/vdr/files/reccmds/removeindex.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/dash
+#!/usr/bin/sh
 find "$1" -maxdepth 1 \( -name "index" -o -name "index.vdr" \) -delete
--- roles/vdr/files/reccmds/removemarks.sh
+++ roles/vdr/files/reccmds/removemarks.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/dash
+#!/usr/bin/sh
 find "$1" -maxdepth 1 \( -name "marks" -o -name "marks.vdr" \) -delete
