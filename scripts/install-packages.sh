d317702de28e35b7890030e542ab5116a7db6174
Alexander Grothe <seahawk1986@gmx.de>
2020-12-24 16:07:52 +0100

    Disable mitogen for now

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -10,15 +10,16 @@ required_packages=(
 apt update
 apt -y install "${required_packages[@]}"
 
-MITOGEN_DIR='/usr/local/lib/mitogen'
-
-if [ ! -e "$MITOGEN_DIR" ]; then
-    git clone 'https://github.com/dw/mitogen.git' "$MITOGEN_DIR"
-else
-    git -C "$MITOGEN_DIR" pull
-fi
-
-if [ -e "${MITOGEN_DIR}/ansible_mitogen/plugins/strategy" ]; then
-        export ANSIBLE_STRATEGY_PLUGINS="${MITOGEN_DIR}/ansible_mitogen/plugins/strategy"
-        export ANSIBLE_STRATEGY=mitogen_linear
-fi
+#MITOGEN_DIR='/usr/local/lib/mitogen'
+#
+#if [ ! -e "$MITOGEN_DIR" ]; then
+#    git clone 'https://github.com/dw/mitogen.git' "$MITOGEN_DIR"
+#else
+#    git -C "$MITOGEN_DIR" pull
+#fi
+#git -C "$MITOGEN_DIR" checkout 4657979 "$MITOGEN_DIR"
+#
+#if [ -e "${MITOGEN_DIR}/ansible_mitogen/plugins/strategy" ]; then
+#        export ANSIBLE_STRATEGY_PLUGINS="${MITOGEN_DIR}/ansible_mitogen/plugins/strategy"
+#        export ANSIBLE_STRATEGY=mitogen_linear
+#fi
