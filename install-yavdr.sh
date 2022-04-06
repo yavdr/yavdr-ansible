e991a8bc7d9e53a994491262c164bbd480183def
seahawk1986 <=>
2022-04-06 12:43:38 +0200

    Fix shebang for bash install scripts
    Ubuntu uses /usr/bin/bash since the https://wiki.debian.org/UsrMerge (Ubuntu
    Disco and later)
    
    If an upgraded installation is used, the package usrmerge can be used to bring
    the shell path to the new standard.

--- install-yavdr.sh
+++ install-yavdr.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 set -e
 
 if (( $EUID != 0 )); then
--- remote-yavdr.sh
+++ remote-yavdr.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 # use this script to configure a remote VDR host instead of localhost
 set -e
 
