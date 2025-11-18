6b2bc201f93f94f48fa4eea68f98aa49fd259382
Alexander Grothe <seahawk1986@gmx.de>
2025-11-18 18:17:50 +0000

    improve typing for satip_facts.py

--- library/satip_facts.py
+++ library/satip_facts.py
@@ -11,18 +11,17 @@ EXAMPLES = '''
 - name: "detect SAT>IP Server on the network"
   action: satip_facts
 
-- debug:
+- ansible.builtin.debug:
     var: satip_devices
 '''
 
-import json
 import socket
-import sys
 import time
+from typing import Any
 import xml.etree.ElementTree as ET
 import requests
 from contextlib import contextmanager
-from ansible.module_utils.basic import *
+from ansible.module_utils.basic import AnsibleModule
 
 SSDP_BIND = "0.0.0.0"
 SSDP_ADDR = "239.255.255.250"
@@ -41,9 +40,12 @@ ssdpRequest = "\r\n".join((
     "\r\n")).encode('utf-8')
 
 @contextmanager
-def socket_manager(*args, **kwargs):
+def socket_manager(family: socket.AddressFamily | int = -1,
+    type: socket.SocketKind | int = -1,
+    proto: int = -1,
+    fileno: int | None = None):
     """provide a context manager for socket"""
-    sock = socket.socket(*args, **kwargs)
+    sock = socket.socket(family, type, proto, fileno)
     sock.setblocking(False)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -59,36 +61,38 @@ def socket_manager(*args, **kwargs):
         sock.close()
 
 
-def parse_satip_xml(data):
+def parse_satip_xml(data: str)-> dict[str, Any]:
     """ Parse SAT>IP XML data.
     Args:
         data (str): XML input data..
     Returns:
         dict: Parsed SAT>IP device name and frontend information.
     """
-    result = {'name': '', 'frontends': {}}
+    result: dict[str, Any] = {'name': '', 'frontends': {}}
     if data:
         root = ET.fromstring(data)
         name = root.find('.//*/{urn:schemas-upnp-org:device-1-0}friendlyName')
+        if name is None or name.text is None:
+            raise ValueError("Invalid SAT>UP device name")
         result['name'] = name.text
         satipcap = root.find('.//*/{urn:ses-com:satip}X_SATIPCAP')
-        if satipcap is None:
+        if satipcap is None or satipcap.text is None:
             raise ValueError("Invalid SAT>IP device description")
-        caps = {}
+        caps: dict[str, int] = {}
         for system in satipcap.text.split(","):
             cap = system.split("-")
             if cap:
                 count = int(cap[1])
                 if cap[0] in caps:
-                    count = count + caps[cap[0]]
+                    count: int = count + caps[cap[0]]
                 caps[cap[0]] = count
         result['frontends'] = caps
     return result
 
 
 def main():
-    description_urls = []
-    device_list = []
+    description_urls: list[str] = []
+    device_list: list[dict[str, Any]] = []
     module = AnsibleModule(argument_spec={}, supports_check_mode=True,)
     with socket_manager(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         # according to Sat>IP Specification 1.2.2, p. 20
