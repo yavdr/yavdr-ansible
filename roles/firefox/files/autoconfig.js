5606f6085f0930f7390dafdb32ca32c702fef0a1
Alexander Grothe <seahawk1986@gmx.de>
2025-12-12 15:02:35 +0100

    Use yavdr-pulse-dbusctl package instead of script from playbook

---
+++ roles/firefox/files/autoconfig.js
@@ -0,0 +1,2 @@
+pref("general.config.filename", "mozilla.cfg"); // Specifies the name of the main config file
+pref("general.config.obscure_value", 0); // Tells Firefox the file is plain text (not obscured)
